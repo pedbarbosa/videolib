81041a127da08f6300b44155e0d25a4f38cb985d
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-12-10 13:26:37 +1000

    Download sample from sample-videos.com

--- spec/videolib_spec.rb
+++ spec/videolib_spec.rb
@@ -4,8 +4,12 @@ require_relative '../lib/mediainfo_wrapper'
 
 describe 'test' do
   it 'Test sample file' do
-    sample = 'spec/sample.mkv'
-    sample_details = [{ show: 'Test', codec: 'HEVC', width: 640, height: 352, size: 85_063_595 }]
+    sample = '/tmp/big_buck_bunny_720p_1mb.mkv'
+    sample_details = [{ show: 'Test', codec: 'MPEG-4 Visual', width: 1280, height: 720, size: 1_052_413 }]
+    unless File.exist?(sample)
+      puts 'Downloading sample file ...'
+      `wget -P /tmp http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_1mb.mkv`
+    end
     expect(scan_episode('Test', sample)).to eql(sample_details)
   end
 end
