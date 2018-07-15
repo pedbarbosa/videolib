395a9581099a1c57fb99204d03e413bb04118b30
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2018-07-15 14:01:00 +1000

    Added file check test

--- lib/mediainfo_wrapper.rb
+++ lib/mediainfo_wrapper.rb
@@ -2,11 +2,16 @@
 
 require 'mediainfo'
 
-def scan_episode(show, name)
-  info = Mediainfo.new name
+def scan_episode(show, filename)
+  return unless file_exists?(filename)
+  info = Mediainfo.new filename
   codec = info.video.format
   width = info.video.width
   height = info.video.height
-  size = File.size(name)
+  size = File.size(filename)
   [show: show, codec: codec, width: width, height: height, size: size]
+end
+
+def file_exists?(filename)
+  File.file?(filename) ? true : false
 end
