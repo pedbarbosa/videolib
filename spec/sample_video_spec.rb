4c7a2e567e1c235ca89e44641c7428ed39931796
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2018-07-15 16:00:28 +1000

    Created method to download sample MKV file

--- spec/sample_video_spec.rb
+++ spec/sample_video_spec.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+def download_sample
+  puts 'Downloading sample file ...'
+  `wget -P /tmp http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_1mb.mkv`
+end
+
 describe 'videolib.rb test' do
   it 'Test sample file' do
     sample = '/tmp/big_buck_bunny_720p_1mb.mkv'
     sample_details = [{ show: 'Test', codec: 'MPEG-4 Visual', width: 1280, height: 720, size: 1_052_413 }]
-    unless File.exist?(sample)
-      puts 'Downloading sample file ...'
-      `wget -P /tmp http://www.sample-videos.com/video/mkv/720/big_buck_bunny_720p_1mb.mkv`
-    end
+    download_sample unless File.exist?(sample)
 
     require_relative '../lib/mediainfo_wrapper'
     episode = scan_episode('Test', sample)
