8d3859dd4d936cd768b3d4868d87902ff795eaac
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2019-09-04 15:29:30 +1000

    Added JSON utils tests

---
+++ spec/lib/json_utils_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/json_utils'
+
+describe 'lib/json_utils.rb test' do
+  let(:test_file) { '/tmp/test.json' }
+  let(:test_hash) { { "a" => "b" } }
+
+  it 'write test' do
+    expect{write_json(test_file, test_hash)}.not_to raise_error
+  end
+
+  before do
+    @read_test = read_json(test_file)
+  end
+
+  it 'read test' do
+    expect(@read_test).to eq(test_hash)
+  end
+end
