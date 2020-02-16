ac7d935e278a193d1c17798a0ce4ea42b447e91f
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2020-02-16 13:19:31 +1000

    Updated cache module

--- lib/config_handler.rb
+++ lib/config_handler.rb
@@ -4,26 +4,24 @@ require 'yaml'
 
 # Video Library config handler
 module ConfigHandler
-  def initialize
-    @config_file = ENV['HOME'] + '/.videolib.yml'
-  end
-
   private
 
   def load_configuration
-    unless File.file?(@config_file)
-      raise ConfigurationFileMissing, "'#{@config_file}' is missing, please check the README file!"
+    config_file = ENV['HOME'] + '/.videolib.yml'
+
+    unless File.file?(config_file)
+      raise ConfigurationFileMissing, "'#{config_file}' is missing, please check the README file!"
     end
 
-    config = YAML.load_file(@config_file)
-    validate_path(config['scan_path'])
+    config = YAML.load_file(config_file)
+    validate_path(config_file, config['scan_path'])
     config
   end
 
-  def validate_path(path)
-    return if File.directory?(path)
+  def validate_path(config_file, scan_path)
+    return if File.directory?(scan_path)
 
-    raise InvalidScanDirectory, "'#{path}' defined in '#{@config_file}' is not a directory!"
+    raise InvalidScanDirectory, "'#{scan_path}' defined in '#{config_file}' is not a directory!"
   end
 
   class ConfigurationFileMissing < StandardError
