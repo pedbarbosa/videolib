227449ffb56b188fa98d1be781e0bd992b374664
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2022-01-09 04:10:13 +1000

    Updated rake test

--- spec/sample_downloader.rb
+++ spec/sample_downloader.rb
@@ -3,15 +3,16 @@
 require 'down'
 
 def media_sample
-  unless File.exist?('/tmp/videolib_sample.mkv')
+  file_path = '/tmp/videolib_sample.mkv'
+  unless File.exist?(file_path)
     puts('Downloading sample file ...')
     tempfile = Down.download('http://mirrors.standaloneinstaller.com/video-sample/small.mkv')
-    FileUtils.mv(tempfile.path, '/tmp/videolib_sample.mkv')
+    FileUtils.mv(tempfile.path, file_path)
   end
 
   { codec: 'V_MPEG4/ISO/AVC',
     height: 320,
-    mtime: 1641664874,
+    mtime: File.mtime(file_path).to_i,
     show: 'test',
     size: 176_123,
     width: 560 }
