33f4d25f1cf515e0bb948addbb890d9cc2c046fa
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2023-02-14 09:30:49 +1000

    Updated media_scanner to clean-up temporary symlink

--- lib/media_scanner.rb
+++ lib/media_scanner.rb
@@ -5,16 +5,12 @@ require_relative '../adapters/mediainfo'
 # Video Library media scanner
 class MediaScanner
   def scan_media_file(file_path, show)
-    media = scan_with_symlink(file_path)
-    file_mtime = File.mtime(file_path).to_i
-    scan_format(media, show, file_mtime)
-  end
-
-  def scan_with_symlink(file_path)
     scan_link = '/tmp/videolib_scan.mkv'
-    File.unlink(scan_link) if File.symlink?(scan_link)
     File.symlink(file_path, scan_link)
-    MediaInfoAdapter.new(scan_link)
+    file_mtime = File.mtime(file_path).to_i
+    result = scan_format(MediaInfoAdapter.new(scan_link), show, file_mtime)
+    File.unlink(scan_link)
+    result
   end
 
   private
